c7bdd94a5f4f9440b5d71809336576012754e26f
reethfx <reethb3rsrk@gmail.com>
2024-08-23 08:55:02 +0200

    home-manager fix

--- nixos/user/reeth/home.nix
+++ nixos/user/reeth/home.nix
@@ -7,11 +7,6 @@
     homeDirectory = "/home/reeth";
     stateVersion = "24.11";
 
-    programs = {
-      home-manager = {
-        enable = true;
-      };
-
       wayland.windowManager.hyprland.enable = true;
 
     };
