e34d6c248a233af4ce09cff53b83acaebea1d78f
reethfx <reethb3rsrk@gmail.com>
2024-08-26 13:53:17 +0200

    rebranch idea

--- nixos/modules/users.nix
+++
@@ -1,9 +0,0 @@
- { config, pkgs, ... }:
- {
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.reeth = {
-    isNormalUser = true;
-    description = "reeth";
-    extraGroups = [ "networkmanager" "wheel" ];
-  };
-}
