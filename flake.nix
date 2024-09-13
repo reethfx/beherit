9f6ecec885bb95fc81b948a08f6ba4319fe575e4
reethfx <reethb3rsrk@gmail.com>
2024-09-13 11:34:56 +0200

    version missmatch fix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
