58831c2a3f78187b6353a99e6a8c46b444669e6a
reethfx <reethb3rsrk@gmail.com>
2024-09-13 17:24:02 +0200

    ç removal

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,6 +1,6 @@
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
-{  inputs, outputs, lib, config, pkgs, ... }:ç
+{  inputs, outputs, lib, config, pkgs, ... }:
 {
   # You can import other home-manager modules here
   imports = [
